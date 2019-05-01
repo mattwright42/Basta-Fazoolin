cfa30994f7c01d929aac8f001ac10adaf4e41288
Matthew Wright <matthewbwright@gmail.com>
2019-05-01 11:06:23 -0700

    creates Menu class and adds Brunch Menu

---
+++ menu.py
@@ -0,0 +1,10 @@
+# Create a Menu class
+
+
+class Menu:
+    def __init__(self, name, items, start_time, end_time):
+        pass
+
+
+brunch = Menu("brunch", {'pancakes': 7.50, 'waffles': 9.00, 'burger': 11.00, 'home fries': 4.50, 'coffee': 1.50, 'espresso': 3.00, 'tea': 1.00, 'mimosa': 10.50, 'orange juice': 3.50
+                         }, 11, 16)
